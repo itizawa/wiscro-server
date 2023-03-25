8256b74bb55ede4288bed1fcf33d4e91bd9f7a75
itizawa <itizawa.pen@gmail.com>
2023-03-25 17:58:05 +0900

    ✨ implement save

--- src/useCases/Page/FetchAndSavePageUseCase/FetchAndSavePageUseCase.ts
+++ src/useCases/Page/FetchAndSavePageUseCase/FetchAndSavePageUseCase.ts
@@ -1,8 +1,10 @@
 import { PageModel } from "~/models/Page";
+import { FetchOgpService } from "~/services/FetchOgpService";
+const fetchOgpService = new FetchOgpService();
 
 export class FetchAndSavePageUseCase {
   async execute(url: string) {
-    // const lienUser = await lineApiService.getProfile(userId);
-    await PageModel.create({ url });
+    const page = await fetchOgpService.fetchOgpByUrl(url);
+    await PageModel.create(page);
   }
 }
