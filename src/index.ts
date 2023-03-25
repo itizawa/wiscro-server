406468a0615623a0cc97228e0abf31fd5577227e
itizawa <itizawa.pen@gmail.com>
2023-03-25 15:31:03 +0900

    ✨ create api

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("こんにちは!");
 });
 
+app.post("/api/line", (req: express.Request, res: express.Response) => {
+  console.log("LINEからのメッセージを受信しました。");
+
+  res.send("LINEからのメッセージ!");
+});
+
 app.listen(3000, () => {
   console.log("ポート3000番で起動しました。");
 });
