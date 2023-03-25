cdac15672cd40335bbdbd4c8081fa1089b45078d
itizawa <itizawa.pen@gmail.com>
2023-03-25 17:54:42 +0900

    🔥

--- src/presentations/controllers/index.ts
+++ src/presentations/controllers/index.ts
@@ -1,8 +1,6 @@
 import * as express from "express";
 import { handleLineRequest } from "./line/handleLineRequest";
-import { listLinks } from "./links/listLinks";
 
 export const setupExpressRoutes = (express: express.Express): void => {
-  express.get("/links", listLinks);
   express.post("/api/line", handleLineRequest);
 };
