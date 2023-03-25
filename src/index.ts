ae73e6c3673104ffad8e15066670b534e274b270
itizawa <itizawa.pen@gmail.com>
2023-03-25 15:25:17 +0900

    🚨

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express from 'express'
-const app: express.Express = express()
-app.get("/", (req:express.Request, res:express.Response)=>{
-    res.send("こんにちは!");
-})
-app.listen(3000,()=>{
-    console.log('ポート3000番で起動しました。')
-})
+import express from "express";
+const app: express.Express = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("こんにちは!");
+});
+
+app.listen(3000, () => {
+  console.log("ポート3000番で起動しました。");
+});
