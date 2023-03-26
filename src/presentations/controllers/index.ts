6aa8cdb8f5c07bce996b7b86cee85ff88471a03f
itizawa <itizawa.pen@gmail.com>
2023-03-26 19:16:55 +0900

    ✨ implement listPages

--- src/presentations/controllers/index.ts
+++ src/presentations/controllers/index.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 import { handleLineRequest } from "./line/handleLineRequest";
+import { listPages } from "./page/listPages";
 
 export const setupExpressRoutes = (express: express.Express): void => {
+  express.get("/api/pages", listPages);
   express.post("/api/line", handleLineRequest);
 };
