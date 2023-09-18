e570728b20a556a008481295bdbc17800b011cc1
itizawa <itizawa.pen@gmail.com>
2023-09-18 18:48:51 +0900

    postQuestion エンドポイントの実装

--- src/presentations/controllers/index.ts
+++ src/presentations/controllers/index.ts
@@ -1,8 +1,10 @@
 import * as express from "express";
 import { handleLineRequest } from "./line/handleLineRequest";
 import { listPages } from "./page/listPages";
+import { postQuestion } from "./question/postQuestion";
 
 export const setupExpressRoutes = (express: express.Express): void => {
   express.get("/api/pages", listPages);
   express.post("/api/line", handleLineRequest);
+  express.post("/api/questions", postQuestion);
 };
