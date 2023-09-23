7191fb80d72a5ad38966484118ff29fe2855a727
itizawa <itizawa.pen@gmail.com>
2023-09-23 15:32:13 +0900

    エンドポイントを実装した

--- src/presentations/controllers/index.ts
+++ src/presentations/controllers/index.ts
@@ -2,9 +2,11 @@ import * as express from "express";
 import { handleLineRequest } from "./line/handleLineRequest";
 import { listPages } from "./page/listPages";
 import { postQuestion } from "./question/postQuestion";
+import { getQuestion } from "./question/getQuestion";
 
 export const setupExpressRoutes = (express: express.Express): void => {
   express.get("/api/pages", listPages);
   express.post("/api/line", handleLineRequest);
+  express.get("/api/questions/:id", getQuestion);
   express.post("/api/questions", postQuestion);
 };
---
+++ src/presentations/controllers/question/getQuestion.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { RetrieveQuestionUseCase } from "~/useCases/Question/RetrieveQuestionUseCase";
+
+const retrieveQuestionUseCase = new RetrieveQuestionUseCase();
+
+export const getQuestion = async (
+  req: Request<object, object, object, { id: string }>,
+  res: Response
+) => {
+  const { id } = req.query;
+  try {
+    const question = await retrieveQuestionUseCase.execute({
+      id,
+    });
+    return res.status(200).json({ question });
+  } catch (error) {
+    return res.status(503).json({ message: "予期せぬエラーが発生しました" });
+  }
+};
