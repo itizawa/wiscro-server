f8c26e79c1d71b2f50658dccf3e4c62f41d6deb3
itizawa <itizawa.pen@gmail.com>
2023-09-23 21:25:45 +0900

    loginRequired

---
+++ src/middlewares/loginRequired.ts
@@ -0,0 +1,23 @@
+import { Response, NextFunction, Request } from "express";
+import { User } from "~/models/User";
+import { logger } from "~/utils/logger";
+
+/**
+ * ログイン状態をチェックするミドルウェア
+ * @param req
+ * @param res
+ * @param next
+ * @returns 403
+ */
+export const loginRequired = (
+  req: Request & { user: User },
+  res: Response,
+  next: NextFunction
+): Response | void => {
+  if (req.user == null) {
+    logger("Error: login required", "error");
+    return res.sendStatus(403);
+  }
+
+  return next();
+};
