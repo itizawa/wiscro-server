dff701e0a8dec505d0958390eaa614df6cbaf47e
itizawa <itizawa.pen@gmail.com>
2023-09-24 17:08:05 +0900

    shema に createdUserId を追加した

--- src/presentations/controllers/question/postQuestion.ts
+++ src/presentations/controllers/question/postQuestion.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
+import { User } from "~/models/User";
 import { CreateQuestionUseCase } from "~/useCases/Question/CreateQuestionUseCase";
 
 const createQuestionUseCase = new CreateQuestionUseCase();
 
-export const postQuestion = async (req: Request, res: Response) => {
+export const postQuestion = async (
+  req: Request & { user: User },
+  res: Response,
+) => {
+  const { user } = req;
   const { title, description } = req.body;
   try {
     const question = await createQuestionUseCase.execute({
+      currentUser: user,
       title,
       description,
     });
