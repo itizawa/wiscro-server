099a2e09ea47f833ec3ff50a623ac0a9fdddb612
itizawa <itizawa.pen@gmail.com>
2023-09-27 08:15:45 +0900

    questionId を answer に追加した

--- src/models/Answer/Answer.ts
+++ src/models/Answer/Answer.ts
@@ -3,12 +3,14 @@ import { Schema, Types, model } from "mongoose";
 export interface Answer {
   url: string;
   createdUserId: Types.ObjectId;
+  questionId: Types.ObjectId;
 }
 
 const userSchema = new Schema<Answer>(
   {
     url: { type: String, required: true },
     createdUserId: { type: Schema.Types.ObjectId, ref: "User" },
+    questionId: { type: Schema.Types.ObjectId, ref: "Question" },
   },
   { timestamps: true },
 );
