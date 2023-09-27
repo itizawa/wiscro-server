488139200b5311753064ccd10d2f924ca37aab5f
itizawa <itizawa.pen@gmail.com>
2023-09-28 00:40:47 +0900

    set ogp

--- src/useCases/Answer/CreateAnswerUseCase/CreateAnswerUseCase.ts
+++ src/useCases/Answer/CreateAnswerUseCase/CreateAnswerUseCase.ts
@@ -1,5 +1,8 @@
 import { Answer, AnswerModel } from "~/models/Answer";
 import { User } from "~/models/User";
+import { FetchOgpService } from "~/services/FetchOgpService";
+
+const fetchOgpService = new FetchOgpService();
 
 export class CreateAnswerUseCase {
   async execute({
@@ -9,7 +12,9 @@ export class CreateAnswerUseCase {
   }: Pick<Answer, "url" | "questionId"> & {
     currentUser: User;
   }): Promise<Answer> {
+    const ogp = await fetchOgpService.fetchOgpByUrl(url);
     return await AnswerModel.create({
+      ...ogp,
       url,
       createdUserId: currentUser._id,
       questionId,
