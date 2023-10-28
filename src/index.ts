a7a4078b87facbcceabc2abeee33b8cac9b480d4
itizawa <itizawa.pen@gmail.com>
2023-10-28 21:21:30 +0900

    set domain

--- src/index.ts
+++ src/index.ts
@@ -80,6 +80,7 @@ export class App {
         resave: false,
         saveUninitialized: true,
         cookie: {
+          domain: "https://www.wiscro.app/",
           path: "/",
           maxAge: 1000 * 60 * 60 * 24 * 30, // 30day
           sameSite: "none",
